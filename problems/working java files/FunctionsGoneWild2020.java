6c221faddaad852930a2a7f0de38b6da5d889156
Sammy H <pokeso123@hotmail.com>
2020-03-07 18:20:04 -0800

    functions gone wild, did a couple functions

--- problems/working java files/FunctionsGoneWild2020.java
+++ problems/working java files/FunctionsGoneWild2020.java
@@ -1,5 +1,8 @@
 import java.util.*;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 /**
  * The test class FunctionsGoneWild2020.
  *
@@ -13,14 +16,12 @@ public class FunctionsGoneWild2020
      */
     public static int f1(int n)
     {
-        if ( n == 1) return 1;
-        if ( n == 2) return 7;
-        if ( n == 3) return 19;
-        if ( n == 4) return 37;
-        if ( n == 5) return 61;
-        if ( n == 6) return 91;
-
-        return -1;
+        if (n == 1) return 1;
+        int accumulator = 0;
+        for (int i = n - 1; i > 0; i--) {
+            accumulator += f1(i);
+        }
+        return (int) (Math.pow(n, 3) - accumulator);
     }
 
     /*
@@ -33,14 +34,34 @@ public class FunctionsGoneWild2020
      *    all values should be positive
      *    
      *    div != 0
-     */   
-    public static int f2(String str ,int div)
-    {
-        if (str.equals("1a1b1") && div == 3) return 34;
-        if (str.equals("ab") && div == 2) return 50;
-        if (str.equals("3a123b") && div == 5) return 20;
-
-        return -1;
+     */
+    public static int f2(String str, int div) {
+        int holeCount = 0;
+        int found = 0;
+        List<Integer> holes = new ArrayList<Integer>();
+        int i = 0;
+        for (char character: str.toCharArray()) {
+            if (character == 'a' || character == 'b') {
+                holeCount++;
+                holes.add(i);
+            }
+            i++;
+        }
+        int maxSize = Integer.parseInt(new String(new char[holeCount]).replace("\0", "9"));
+        for (int k = 0; k < maxSize + 1; k++) {
+            char[] nums = String.format("%1$" + holeCount + "s", String.valueOf(k)).replace(' ', '0').toCharArray();
+            char[] newTest = str.toCharArray();
+            int b = 0;
+            for (Iterator<Integer> it = holes.iterator(); it.hasNext(); ) {
+                int hole = it.next();
+                newTest[hole] = nums[b];
+                b++;
+            }
+            if ((Integer.parseInt(new String(newTest)) % div) == 0) {
+                found++;
+            }
+        }
+        return found;
     }
 
     /*
@@ -81,14 +102,12 @@ public class FunctionsGoneWild2020
         return -1;
     }
 
-    public static double f6(double x, double y, double z)
-    {
-        if (Math.abs(x - 4*Math.PI/5.) < 0.002 && Math.abs(y - 3.) < 0.002 && Math.abs(z - 3*Math.PI/2) < 0.002)
-            return 35.922375;
-        if (Math.abs(x - 4*Math.PI/5.) < 0.002 && Math.abs(y - 2.) < 0.002 && Math.abs(z - Math.PI/3) < 0.002)
-            return 65.250785;
-
-        return -25*Math.random();
+    public static double f6(double x, double y, double z) {
+        if (Math.sin(x) > Math.cos(z/y)) {
+            return 10 * Math.log10(Math.pow(Math.PI, x+z));
+        } else {
+            return 25 * Math.log(Math.pow(y, Math.E + z));
+        }
     }
 
     /*
@@ -124,8 +143,13 @@ public class FunctionsGoneWild2020
     public static boolean f9(boolean x, boolean y, boolean z)
     {
         if (!x && !y && !z) return true;
-
-        return Math.random() < 0.5;
+        if (!x && !y && z) return false;
+        if (!x && y && !z) return false;
+        if (!x && y && z) return true;
+        if (x && !y && !z) return true;
+        if (x && !y && z) return false;
+        if (x && y && !z) return true;
+        if (x && y && z) return false;
     }
 
     public static boolean f10(boolean a, boolean b, boolean c, boolean d)
--- src/com/company/ExampleTest.java
+++ src/com/company/ExampleTest.java
@@ -1,7 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ExampleTest {
+    public static boolean f9(boolean x, boolean y, boolean z)
+    {
+        if (!x && !y && !z) return true;
+        if (!x && !y && z) return false;
+        if (!x && y && !z) return false;
+        if (!x && y && z) return true;
+        if (x && !y && !z) return true;
+        if (x && !y && z) return false;
+        if (x && y && !z) return true;
+        if (x && y && z) return false;
+        return false;
+    }
     public static void main(String[] args) {
-        System.out.println("GitHub Workflow better work >:(");
+        System.out.println(f9(false, false, false));
     }
 }
