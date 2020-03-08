c7e0a14d338d3a791be2c067fd8e1a26bc3c18df
Sammy H <pokeso123@hotmail.com>
2020-03-07 19:07:13 -0800

    add em up

--- problems/working java files/AddEmUp.java
+++ problems/working java files/AddEmUp.java
@@ -7,6 +7,8 @@ import java.lang.Math;
  * @author  Don Allen
  * @version 2020 Wittry Contest
  */
+import java.util.*;
+
 public class AddEmUp
 {
     /*
@@ -29,25 +31,15 @@ public class AddEmUp
     public List<Integer> rowSum(int row)
     {
         List<Integer> ans = new ArrayList<Integer>();
+        for (int num: numbers[row]) {
+            for (int num2: numbers[row]) {
+                if (!ans.contains(num2 + num) && num != num2) {
+                    ans.add(num2 + num);
+                }
+            }
 
-        if ( numbers[0][0] == 3)
-        {
-            if ( numbers[0][1] == 6)
-            {
-                ans.add(new Integer(9));
-                ans.add(new Integer(11));
-                ans.add(new Integer(14));
-                return ans;
-            }
-            if ( numbers[0][1] == -1)
-            {
-                ans.add(new Integer(2));
-                ans.add(new Integer(6));
-                return ans;
-            }
         }
-
-        return null;
+        return ans;
     }
 
     /*
@@ -55,45 +47,62 @@ public class AddEmUp
      *  (For this method, repeated sums count multiple times.)
      *  Remember, a number, x, is even if x % 2 == 0.
      *  This method returns:
-     *    “EVEN” if there exist more even numbers in the List of all possible sum of two entries in a given row.
-     *    “ODD” if there exist more odd numbers in the List of all possible sum of two entries in a given row.
-     *    “NEITHER” if there exist the same number of even and odd numbers in the List of all possible sum of two entries in a given row.
+     *    ï¿½EVENï¿½ if there exist more even numbers in the List of all possible sum of two entries in a given row.
+     *    ï¿½ODDï¿½ if there exist more odd numbers in the List of all possible sum of two entries in a given row.
+     *    ï¿½NEITHERï¿½ if there exist the same number of even and odd numbers in the List of all possible sum of two entries in a given row.
      */
     public String getState(int row)
     {
-        //      { 3, 6, 8}, {2, 12, 7}, {8, 6, 4}};
-        if ( numbers[0][0] == 3)
-        {
-            if ( numbers[0][1] == 6)
-            {
-                if (row == 0) return "ODD";
-                if (row == 1) return "ODD";
-                if (row == 2) return "EVEN";
-            }
-            if ( numbers[0][1] == -1)
-            {
-                if (row == 0) return "EVEN";
-                if (row == 1) return "ODD";
+        int even = 0;
+        int odd = 0;
+        List<Integer> sums = this.rowSum(row);
+        for (Iterator<Integer> it = sums.iterator(); it.hasNext(); ) {
+            Integer num = it.next();
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
             }
         }
-        return null;
+        if (even > odd) return "EVEN";
+        if (odd > even) return "ODD";
+        return "NEITHER";
     }
 
     /*
-     *   returns a List of all values that are contained in every List returned by rowSum(k) method, 0 <= k < numbers.length 
+     *   returns a List of all values that are contained in every List returned by rowSum(k) method, 0 <= k < numbers.length
      *   for all rows in the 2D array.
      *
      *   That is, a List of all values that would be contain in the rowSum(k) method for all possible values of k.
      */
     public List<Integer> commonSum()
     {
-        //      { 3, 6, 8}, {2, 12, 7}, {8, 6, 4}};
-        if ( numbers[0][0] == 3)
-        {
-            List<Integer> ans = new ArrayList<Integer>();
-            ans.add(new Integer(14));
-            return ans;
+        List<List<Integer>> sums = new ArrayList<List<Integer>>();
+        List<Integer> commonSums = new ArrayList<Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            sums.add(this.rowSum(i));
         }
-        return null;
+        List<Integer> allSums = new ArrayList<Integer>();
+        for (Iterator<List<Integer>> it = sums.iterator(); it.hasNext(); ) {
+            List<Integer> sum = it.next();
+            allSums.addAll(sum);
+        }
+        HashMap<Integer, Integer> occurences = new HashMap<>();
+        for (Iterator<Integer> it = allSums.iterator(); it.hasNext(); ) {
+            occurences.put(it.next(), 0);
+        }
+        for (Iterator<Integer> it = allSums.iterator(); it.hasNext(); ) {
+            Integer next = it.next();
+            occurences.put(next, occurences.get(next) + 1);
+        }
+        Iterator it = occurences.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if ((Integer) pair.getValue() == numbers.length) {
+                commonSums.add((Integer) pair.getKey());
+            }
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        return commonSums;
     }
 }
--- src/com/company/ExampleTest.java
+++ src/com/company/ExampleTest.java
@@ -1,23 +1,122 @@
 package com.company;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class ExampleTest {
-    public static boolean f9(boolean x, boolean y, boolean z)
+import java.lang.*;
+import java.util.*;
+import java.lang.Math;
+/**
+ * The test class .
+ *
+ * @author  Don Allen
+ * @version 2020 Wittry Contest
+ */
+class AddEmUp
+{
+    /*
+     *    All 2D arrays will be rectangular.  That is, each row in the array will be the same length
+     *
+     *    i.e., number[m].length = number[n].length, 0 <= m,n < number.length
+     */
+    private int[][] numbers;
+
+    public AddEmUp(int[][] num)
     {
-        if (!x && !y && !z) return true;
-        if (!x && !y && z) return false;
-        if (!x && y && !z) return false;
-        if (!x && y && z) return true;
-        if (x && !y && !z) return true;
-        if (x && !y && z) return false;
-        if (x && y && !z) return true;
-        if (x && y && z) return false;
-        return false;
+        numbers = num;
     }
+
+    /*
+     *    returns a list of all possible sums using two entries from the given row.
+     *
+     *    The returned List<Integer> should have no repeated values
+     */
+    public List<Integer> rowSum(int row)
+    {
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int num: numbers[row]) {
+            for (int num2: numbers[row]) {
+                if (!ans.contains(num2 + num) && num != num2) {
+                    ans.add(num2 + num);
+                }
+            }
+
+        }
+        return ans;
+    }
+
+    /*
+     *  determines the state of row in the 2D array.
+     *  (For this method, repeated sums count multiple times.)
+     *  Remember, a number, x, is even if x % 2 == 0.
+     *  This method returns:
+     *    �EVEN� if there exist more even numbers in the List of all possible sum of two entries in a given row.
+     *    �ODD� if there exist more odd numbers in the List of all possible sum of two entries in a given row.
+     *    �NEITHER� if there exist the same number of even and odd numbers in the List of all possible sum of two entries in a given row.
+     */
+    public String getState(int row)
+    {
+        int even = 0;
+        int odd = 0;
+        List<Integer> sums = this.rowSum(row);
+        for (Iterator<Integer> it = sums.iterator(); it.hasNext(); ) {
+            Integer num = it.next();
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        if (even > odd) return "EVEN";
+        if (odd > even) return "ODD";
+        return "NEITHER";
+    }
+
+    /*
+     *   returns a List of all values that are contained in every List returned by rowSum(k) method, 0 <= k < numbers.length
+     *   for all rows in the 2D array.
+     *
+     *   That is, a List of all values that would be contain in the rowSum(k) method for all possible values of k.
+     */
+    public List<Integer> commonSum()
+    {
+        List<List<Integer>> sums = new ArrayList<List<Integer>>();
+        List<Integer> commonSums = new ArrayList<Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            sums.add(this.rowSum(i));
+        }
+        List<Integer> allSums = new ArrayList<Integer>();
+        for (Iterator<List<Integer>> it = sums.iterator(); it.hasNext(); ) {
+            List<Integer> sum = it.next();
+            allSums.addAll(sum);
+        }
+        HashMap<Integer, Integer> occurences = new HashMap<>();
+        for (Iterator<Integer> it = allSums.iterator(); it.hasNext(); ) {
+            occurences.put(it.next(), 0);
+        }
+        for (Iterator<Integer> it = allSums.iterator(); it.hasNext(); ) {
+            Integer next = it.next();
+            occurences.put(next, occurences.get(next) + 1);
+        }
+        Iterator it = occurences.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if ((Integer) pair.getValue() == numbers.length) {
+                commonSums.add((Integer) pair.getKey());
+            }
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+        return commonSums;
+    }
+}
+
+public class ExampleTest {
+    private static int[][] nums = { { 3, 6, 8}, {2, 12, 7}, {8, 6, 4}};
+    private static int[][] another =  { { 3, -1, 2, 0}, {2, 2, 1, 2} };
     public static void main(String[] args) {
-        System.out.println(f9(false, false, false));
+        AddEmUp test = new AddEmUp(nums);
+        System.out.println(test.commonSum());
     }
 }
